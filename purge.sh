589774db399b2f8034e04163110ba8e83d5b9662
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2021-07-18 22:56:08 +0400

    Add all files

---
+++ purge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+    echo "Purging all cache"
+    rm -rf /var/cache/cdn/*
+else
+    echo "Purging $1"
+    FILE=`echo -n "$1" | md5sum | awk '{print $1}'`
+        FULLPATH=/var/cache/cdn/${FILE:31:1}/${FILE:29:2}/${FILE}
+    rm -f "${FULLPATH}"
+fi
